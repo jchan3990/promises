06f038fe2b7c4523507c49ffc3680aac4b551502
jchan3990 <jonchan3990@gmail.com>
2020-06-02 12:26:04 -0700

    Finished callbackReview

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,29 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, cb) {
   // TODO
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      cb(err, data);
+    } else {
+      // Split data into lines
+      data = data.toString().split('\n');
+      cb(null, data[0]);
+    }
+  })
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = function (url, cb) {
   // TODO
+  request(url, (err, res, body) => {
+    if (err) {
+      cb(err);
+    } else {
+      cb(err, res.statusCode);
+    }
+  })
 };
 
 // Export these functions so we can test them and reuse them in later exercises
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -11,6 +11,7 @@ var Promise = require('bluebird');
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
   // TODO
+
 };
 
 // This function should retrieve the status code of a GET request to `url`
