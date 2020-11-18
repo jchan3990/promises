59dce3201a0c5cda365934b28387d7b391da6fed
jchan3990 <jonchan3990@gmail.com>
2020-11-17 20:06:45 -0800

    Advanced collections

--- exercises/advanced/collections.js
+++ exercises/advanced/collections.js
@@ -8,8 +8,21 @@
  *    4. Writes the new file to the file located at `writePath`
  */
 
+ const Promise = require('bluebird');
+ const fs = require('fs');
+ const { pluckFirstLineFromFileAsync } = require('../bare_minimum/promiseConstructor.js');
 
 var combineFirstLineOfManyFiles = function(filePaths, writePath) {
+  let promises = filePaths.map(file => pluckFirstLineFromFileAsync(file));
+
+  return new Promise.all(promises)
+    .then(lines => {
+      for (let i = 0; i < lines.length; i++) {
+        if (i < lines.length - 1) fs.appendFileSync(writePath, `${lines[i]}\n`);
+        else fs.appendFileSync(writePath, `${lines[i]}`);
+      }
+    })
+
  // TODO
 };
 
